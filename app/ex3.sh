a3e8ca747c9aaf93b23bd656e5a59437b6037436
HabibAroua <habibha.aroua82@gmail.com>
2017-12-26 21:10:25 +0100

    exercice 3 tp

--- app/ex3.sh
+++ app/ex3.sh
@@ -1,12 +1,10 @@
-if test -f $1
+if test -d $1
 then
-        echo "est un fichier"
-elif test -d $1
+        echo "est un repertoire $1" 
+elif test -f $1
 then
-        echo "est un repertoire"
-elif test -b $1
+        echo `ls -l $1`
+elif test -h $1
 then
-        echo "est un bloc "
-else
-        echo "Aucun des tests précédents n'a été vérifié"
+        echo `stat -c %x $1 | awk -F: '{print $1}'`
 fi
